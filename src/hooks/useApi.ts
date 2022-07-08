006849681456c7e31bfd8d580b21f7b1c00a5417
Ali Hoseini <Ali4bcup@gmail.com>
2022-07-08 22:52:57 +0430

    created api call hook (custom hook)

---
+++ src/hooks/useApi.ts
@@ -0,0 +1,55 @@
+import { useState, useEffect } from "react";
+
+interface IApiSingleData {
+  userId: number;
+  id?: number;
+  title: string;
+  body: string;
+}
+
+
+export const useGetList = () => {
+  const [data, setData] = useState<IApiSingleData[]>();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    setLoading(true);
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.json())
+      .then((data) => setData(data))
+      .then(() => setLoading(false))
+      .catch((error) => {
+        setLoading(false);
+        setError(error);
+      });
+  }, []);
+
+  return { data, loading, error };
+};
+export const useCreate = ({ title, body, userId }: IApiSingleData) => {
+  const [data, setData] = useState<IApiSingleData>();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState();
+
+  setLoading(true);
+  fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    body: JSON.stringify({
+      title: { title },
+      body: { body },
+      userId: { userId },
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => setData(data))
+    .catch((error) => {
+      setLoading(false);
+      setError(error);
+    });
+
+  return { data, loading, error };
+};
